47d4fb9dc7f92d92753afd1848f9b0782a943867
mmilde <mmilde@ini.uzh.ch>
2019-02-20 13:56:56 +0100

    Update init and dot files

--- install_packages.sh
+++ install_packages.sh
@@ -7,7 +7,22 @@ sudo apt-get upgrade
 # install relevant programs
 sudo apt-get install -y terminator inkscape htop ipython3 vlc vim thunderbird chromium-browser \
     texlive-latex-extra latexmk texlive-bibtex-extra texlive texmaker git subversion zathura \
-    numix-icon-theme numix-icon-theme-circle openvpn sublime-text xclip
+    numix-icon-theme numix-icon-theme-circle openvpn sublime-text xclip build-essential
+
+# install emacs
+sudo apt-get build-dep emacs24
+cd ~/Downloads/
+wget http://ftp.gnu.org/gnu/emacs/emacs-25.3.tar.gz
+tar -xvf emacs-25.3.tar.gz
+./configure
+make
+sudo make install
+
+git clone https://github.com/syl20bnr/spacemacs.git ~/.emacs.d
+cd ~/.emacs.d
+git checkout develop
+cp ~/Repositories/dot-files/.spacemacs ~/
+
 # create installation dir
 sudo mkdir -p /opt/franz
 
@@ -29,14 +44,14 @@ Type=Application
 Categories=Messaging,Internet
 EOF"
 
-cp /Repositories/dot-files/.bash_aliases ~/
+mkdir ~/Repositories
+cd ~/Repositories/
+git clone 
+cp ~/Repositories/dot-files/.bash_aliases ~/
 cp -r /Repositories/dot-files/.config ~/
-cp -r /media/schlowmo/Ellesmera/BackUp/.thunderbird ~/
+cp -r /Repositories/dot-files/.thunderbird ~/
 
 source ~/.bashrc
-# install python related libraries via apt-get
-sudo apt-get install -y python3-pip python3-setuptools python3-matplotlib cython\
-    python3-yaml python3-serial ant python3-opencv python3-opengl
 
 # install libraries and codecs
 sudo apt-get install -y libusb-dev libopencv-dev mesa-utils mesa-common-dev \
@@ -44,24 +59,17 @@ sudo apt-get install -y libusb-dev libopencv-dev mesa-utils mesa-common-dev \
     libboost-all-dev libgflags-dev libgoogle-glog-dev liblmdb-dev libprotobuf-dev libleveldb-dev \
     libsnappy-dev libhdf5-serial-dev protobuf-compiler g++
 
-# install python related packages via pip
-pip3 install easydict brian2 pyqt5 pyqtgraph numpy scipy seaborn sparse h5py
 
-# install nvidia drivers
-sudo apt-get install nvidia-384-dev
+# install python related libraries via apt-get
+# install miniconda and create python3.7 env
+conda create -n teili python=3.5
+conda create -n python_latest python=3.7.1
 
-# install opencv
-# sudo apt-get install build-essential cmake pkg-config libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libgtk-3-dev libatlas-base-dev gfortran python2.7-dev python3.5-dev
-# cd ~ && wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.3.0.zip
-# unzip opencv.zip
-# wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.3.0.zip
-# unzip opencv_contrib.zip
-# cd ~/opencv-3.3.0/
-# mkdir build && cd build
-# cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D INSTALL_PYTHON_EXAMPLES=ON -D INSTALL_C_EXAMPLES=OFF -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.3.0/modules -D BUILD_EXAMPLES=ON ..
-# make -j4
-# sudo make install
-# sudo ldconfig
+source activate teili
+cd ~/Repositories/
+pip install teili/
+pip install OCTA/
+
 
 # Install libcaer
 cd ~/Repositories/libcaer
--- setup_repositories.sh
+++ setup_repositories.sh
@@ -8,13 +8,11 @@ sudo chown -R schlowmo:schlowmo Repositories
 cd ~/Repositories
 git config --global user.name "Moritz Milde"
 git config --global user.email "mmilde@ini.uzh.ch"
-git clone git@github.com:SensorsINI/jaer.git
-cd jaer && ant jar
+# git clone git@github.com:SensorsINI/jaer.git
+# cd jaer && ant jar
 cd /etc/udev/rules.d/ 
 sudo wget https://github.com/inilabs/devices-bin/blob/master/drivers/linux/udev-rules/65-inilabs.rules
 cd ~/Repositories
-git clone https://github.com/inilabs/caer.git
-git clone git@code.ini.uzh.ch:ncs/NCSBrian2Lib.git
+git clone git@code.ini.uzh.ch:ncs/teili.git
 git clone git@code.ini.uzh.ch:mmilde/OCTA.git
-git clone git@code.ini.uzh.ch:ncs/libs/ncs_brian.git
 git clone https://github.com/inilabs/libcaer.git
