67a4f297ba806edc6bf67270614eb1a565162a94
Moritz Milde <m.milde@westernsydney.edu.au>
2019-11-06 11:53:59 +1100

    updates scripts for 18.04

--- install_packages.sh
+++ install_packages.sh
@@ -5,32 +5,50 @@ sudo apt-get upgrade
 # install relevant programs
 sudo apt-get install -y terminator inkscape htop ipython3 vlc vim chromium-browser \
     texlive-latex-extra latexmk texlive-bibtex-extra texlive git subversion zathura \
-    numix-icon-theme numix-icon-theme-circle openvpn xclip build-essential  \
-    lemonbar i3 i3lock i3lock-fancy lm-sensors 
+    numix-icon-theme numix-icon-theme-circle openvpn xclip build-essential \
+    lemonbar i3 i3lock i3lock-fancy lm-sensors zsh
 
-# Maybe change i3 to i3-gaps-wm from
-# sudo add-apt-repository ppa:kgilmer/speed-ricer
+# install libraries and codecs
+sudo apt-get install -y libusb-dev libopencv-dev mesa-utils mesa-common-dev \
+    freeglut3-dev pkg-config build-essential cmake libatlas-base-dev --no-install-recommends \
+    libboost-all-dev libgflags-dev libgoogle-glog-dev liblmdb-dev libprotobuf-dev libleveldb-dev \
+    libsnappy-dev libhdf5-serial-dev protobuf-compiler g++ libgtk-3-dev libgif-dev \
+    libxpm-dev libtinfo-dev fonts-powerline curl
 
-# cd ~/Repositories/
-# git clone git clone https://github.com/meskarune/i3lock-fancy.git
-# cd i3lock-fancy
-# sudo make install
+chsh -s $(which zsh)
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 cd ~/Repositories/
 git clone https://github.com/i3-gnome/i3-gnome.git
 cd i3-gnome/
 sudo make install
 
+git clone git://github.com/sigurdga/gnome-terminal-colors-solarized.git ~/.solarized
+cd .solarized/
+./install.sh
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+cd /tmp	
+git clone https://github.com/ryanoasis/nerd-fonts.git
+./nerds-fonts/install.sh
+cd
+
 # install emacs
-sudo sed -Ei 's/^# deb-src /deb-src /' /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get build-dep emacs24
-cd ~/Downloads/
-wget http://ftp.gnu.org/gnu/emacs/emacs-25.3.tar.gz
-tar -xvf emacs-25.3.tar.gz
-./configure
-make
-sudo make install
+sudo add-apt-repository ppa:kelleyk/emacs
+sudo apt update
+sudo apt install emacs26
 
 git clone https://github.com/syl20bnr/spacemacs.git ~/.emacs.d
 cd ~/.emacs.d
@@ -42,6 +60,7 @@ sudo mkdir -p /opt/franz
 
 #install franz
 wget -qO- https://github.com/meetfranz/franz/releases/download/v5.3.2/franz-5.3.2.tar.gz | sudo tar xvz -C /opt/franz/
+sudo mv franz-5.3.2/ Franz
 
 # add app icon
 sudo wget "https://cdn-images-1.medium.com/max/360/1*v86tTomtFZIdqzMNpvwIZw.png" -O /opt/franz/franz-icon.png
@@ -51,7 +70,7 @@ sudo bash -c "cat <<EOF > /usr/share/applications/franz.desktop
 [Desktop Entry]
 Name=Franz
 Comment=
-Exec=/opt/franz/Franz
+Exec=/opt/franz/Franz/franz
 Icon=/opt/franz/franz-icon.png
 Terminal=false
 Type=Application
@@ -62,36 +81,32 @@ cp ~/Repositories/dot-files/.bash_aliases ~/
 cp -r ~/Repositories/dot-files/.config ~/
 cp -r ~/Repositories/dot-files/.ipython ~/
 cp ~/Repositories/dot-files/.vimrc ~/
+cp ~/Repositories/dot-files/.zshrc ~/
 source ~/.bashrc
-
-# install libraries and codecs
-sudo apt-get install -y libusb-dev libopencv-dev mesa-utils mesa-common-dev \
-    freeglut3-dev pkg-config build-essential cmake libatlas-base-dev --no-install-recommends \
-    libboost-all-dev libgflags-dev libgoogle-glog-dev liblmdb-dev libprotobuf-dev libleveldb-dev \
-    libsnappy-dev libhdf5-serial-dev protobuf-compiler g++
-
 
 # install python related libraries via apt-get
 # install miniconda and create python3.7 env
-cd ~/tmp/
+cd /tmp
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda-latest-Linux-x86_64.sh
 ./Miniconda-latest-Linux-x86_64.sh
 
 conda create -n teili python=3.5
-conda create -n py-late python=3.7.1
+conda create -n 3.7 python=3.7.1
 
 # Install libcaer
-git clone https://github.com/inilabs/libcaer.git
-cd ~/Repositories/libcaer
-sudo apt-get install build-essential cmake pkg-config libusb-1.0-0-dev libserial-dev
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DENABLE_OPENCV=1 .
-make -j4
-sudo make install
+# cd ~/Repositories
+# git clone https://github.com/inilabs/libcaer.git
+# cd ~/Repositories/libcaer
+# sudo apt-get install build-essential cmake pkg-config libusb-1.0-0-dev libserial-dev
+# cmake -DCMAKE_INSTALL_PREFIX=/usr -DENABLE_OPENCV=1 .
+# make -j4
+# sudo make install
 
 # Get desktop background wallpaper
 cd ~/Pictures/
 mkdir Wallpapers/
+cd Wallpapers/
 wget -O tree.png https://newevolutiondesigns.com/images/freebies/nature-hd-background-4.jpg
 wget -O heart.jpg https://www.dropbox.com/s/iui4icdko8lufc3/heart.jpg?dl=1
 wget -O triangle.jpg https://www.dropbox.com/s/o5b96rd8xyratei/art-digital-miscellaneous-modern-abstract-minimalist-wallpaper-hipster-triangle-wallpapers-images.jpg?dl=1
--- setup_repositories.sh
+++ setup_repositories.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ssh-keygen -t rsa -b 4096 -C "mmilde@ini.uzh.ch"
+ssh-keygen -t rsa -b 4096 -C "m.milde@westernsydney.edu.au"
 cat ~/.ssh/id_rsa.pub | cop
 read -p "SSH PUB ID has been copied to clip board. Please add it to Github and Gitlab and then press [Enter] key to continue..."
 # Set up standard repositories
@@ -7,14 +7,15 @@ mkdir ~/Repositories
 sudo chown -R schlowmo:schlowmo Repositories
 cd ~/Repositories
 git config --global user.name "Moritz Milde"
-git config --global user.email "mmilde@ini.uzh.ch"
-# git clone git@github.com:SensorsINI/jaer.git
-# cd jaer && ant jar
+git config --global user.email "m.milde@westernsydney.edu.au"
+
+
 cd /etc/udev/rules.d/
 sudo wget https://github.com/inilabs/devices-bin/blob/master/drivers/linux/udev-rules/65-inilabs.rules
 cd ~/Repositories
 git clone git@code.ini.uzh.ch:ncs/teili.git
 git clone git@code.ini.uzh.ch:mmilde/OCTA.git
+git clone git@github.com:schlowm0/sense8.git
 
 source activate teili
 cd ~/Repositories/
